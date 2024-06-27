08883bac62216a3430a964a8a18b9ca8460a8904
xesam <xesam@outlook.com>
2024-06-27 10:03:43 +0800

    sample-server /fix

---
+++ sample-server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const messages = ['Hello', 'Are ', 'you Ok?'];
+
+server.get('/stream', (req, res) => {
+    console.log('/stream');
+    res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    let count = 0;
+    const timer = setInterval(() => {
+        if (count === messages.length) {
+            clearInterval(timer);
+            res.end('data: Finished\n\n');
+        } else {
+            res.write(`data: ${messages[count]}\n\n`);
+        }
+        count++;
+    }, 1000);
+});
+
+server.get('/stream-timeout', (req, res) => {
+});
+
+server.get('/stream-hang-up', (req, res) => {
+    console.log('/stream-timeout');
+    res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    let count = 0;
+    const timer = setInterval(() => {
+        if (count === messages.length) {
+            clearInterval(timer);
+        } else {
+            res.write(`data: ${messages[count]}\n\n`);
+        }
+        count++;
+    }, 1000);
+});
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
