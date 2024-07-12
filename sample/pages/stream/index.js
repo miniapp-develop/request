048acc757bb716ed871fa16e6fefa9032745df51
xesam <xesam@outlook.com>
2024-07-12 14:00:49 +0800

    sample /fix

--- sample/pages/stream/index.js
+++ sample/pages/stream/index.js
@@ -1,19 +1,24 @@
 import request from '../../app.request';
 import * as streams from './streams';
 
+const pageRequest = request.create().addRequestInterceptor(req => {
+    console.log(`[Page Streams] res = `, req);
+    return req;
+});
+
 Page({
     onLoad(query) {
     },
     onTapStreamWithHeader(e) {
-        streams.streamWithHeader(request);
+        streams.streamWithHeader(pageRequest);
     },
     onTapStreamWithoutHeader(e) {
-        streams.streamWithoutHeader(request);
+        streams.streamWithoutHeader(pageRequest);
     },
     onTapStreamTimeout(e) {
-        streams.streamTimeout(request);
+        streams.streamTimeout(pageRequest);
     },
     onTapPlainText(e) {
-        streams.requestPlainText(request);
+        streams.requestPlainText(pageRequest);
     }
 });
